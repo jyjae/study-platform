141d2bbe0044d6446a29a1e02ef9eead337d662e
dmin3 <ekdrnzld@gmail.com>
2022-08-02 22:07:25 +0900

    StudyNotice 엔티티 생성

---
+++ src/main/java/com/example/studyplatform/domain/studyNotice/StudyNoticeRepository.java
@@ -0,0 +1,6 @@
+package com.example.studyplatform.domain.studyNotice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyNoticeRepository extends JpaRepository<StudyNotice, Long> {
+}
