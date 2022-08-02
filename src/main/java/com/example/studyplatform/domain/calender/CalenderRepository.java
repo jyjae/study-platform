f4989dc23a49fd2460479b498370e0913c10b0a7
dmin3 <ekdrnzld@gmail.com>
2022-08-02 22:08:44 +0900

    Calender 엔티티 생성

---
+++ src/main/java/com/example/studyplatform/domain/calender/CalenderRepository.java
@@ -0,0 +1,6 @@
+package com.example.studyplatform.domain.calender;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalenderRepository extends JpaRepository<Calender, Long> {
+}
