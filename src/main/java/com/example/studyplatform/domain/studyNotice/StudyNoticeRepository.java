646955d2cc9c3cf372e8f2272c9108c19af6fac1
dmin3 <ekdrnzld@gmail.com>
2022-08-07 17:24:10 +0900

    fix: StudyNotice 메소드 추가

--- src/main/java/com/example/studyplatform/domain/studyNotice/StudyNoticeRepository.java
+++ src/main/java/com/example/studyplatform/domain/studyNotice/StudyNoticeRepository.java
@@ -2,5 +2,10 @@ package com.example.studyplatform.domain.studyNotice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudyNoticeRepository extends JpaRepository<StudyNotice, Long> {
+
+    Optional<StudyNotice> findByIdAndUserId(Long studyNoticeId, Long userId);
+
 }
