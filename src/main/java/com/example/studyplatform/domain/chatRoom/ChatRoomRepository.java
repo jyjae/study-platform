6e29216cc92f4de103a026bb7261e45e98118de2
yeonjaery <yeonjaery@gmail.com>
2022-08-17 02:53:19 +0900

    feat: 일대일 채팅방 생성, 그룹 채팅방 생성 repository 추가

--- src/main/java/com/example/studyplatform/domain/chatRoom/ChatRoomRepository.java
+++ src/main/java/com/example/studyplatform/domain/chatRoom/ChatRoomRepository.java
@@ -1,6 +1,12 @@
 package com.example.studyplatform.domain.chatRoom;
 
+import com.example.studyplatform.constant.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findByRoomHashCodeAndStatus(int roomHashCode, Status active);
+
+    Optional<ChatRoom> findByIdAndStatus(Long chatRoomId, Status active);
 }
