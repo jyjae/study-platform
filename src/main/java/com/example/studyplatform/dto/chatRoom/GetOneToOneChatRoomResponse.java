62c398056cd53c948758ace39d2e3da9f5ac5625
yeonjaery <yeonjaery@gmail.com>
2022-08-19 03:52:35 +0900

    feat: 일대일 채팅, 그룹 채팅 조회 및 삭제, 다른 유저 정보 조회 관련 dto 추가

---
+++ src/main/java/com/example/studyplatform/dto/chatRoom/GetOneToOneChatRoomResponse.java
@@ -0,0 +1,37 @@
+package com.example.studyplatform.dto.chatRoom;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class GetOneToOneChatRoomResponse {
+    private String roomName;
+    private String chatRoomId;
+    private String lastMessage;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createAt;
+    private String dayBefore;
+    private int unreadCount;
+
+    public GetOneToOneChatRoomResponse(
+            String roomName,
+            String chatRoomId,
+            String lastMessage,
+            LocalDateTime createAt,
+            String dayBefore,
+            int unreadCount
+    ) {
+        this.roomName = roomName;
+        this.chatRoomId = chatRoomId;
+        this.lastMessage = lastMessage;
+        this.createAt = createAt;
+        this.dayBefore = dayBefore;
+        this.unreadCount = unreadCount;
+    }
+}
