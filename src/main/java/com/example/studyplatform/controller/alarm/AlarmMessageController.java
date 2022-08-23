8e89382cfb88d52f9c833f737452e5e0c9540a59
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2022-08-23 12:50:21 +0900

    feat : 스터디 공지 알림 API

--- src/main/java/com/example/studyplatform/controller/alarm/AlarmMessageController.java
+++ src/main/java/com/example/studyplatform/controller/alarm/AlarmMessageController.java
@@ -5,16 +5,19 @@ import com.example.studyplatform.dto.chat.ChatMessageRequest;
 import com.example.studyplatform.dto.comment.PostCommentRequest;
 import com.example.studyplatform.service.chat.ChatMessageService;
 import com.example.studyplatform.service.comment.CommentService;
+import com.example.studyplatform.service.studyNotice.StudyNoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class AlarmMessageController {
     private final ChatMessageService chatMessageService;
     private final CommentService commentService;
+    private final StudyNoticeService studyNoticeService;
 
     // 채팅 알람
     @MessageMapping("/chat/alarm")
@@ -27,5 +30,14 @@ public class AlarmMessageController {
     public void commentAlarm(PostCommentRequest postCommentRequest,
                              @AuthenticationPrincipal User user) {
         commentService.sendCommentAlarm(postCommentRequest, user);
+    }
+
+    // 스터디 공지 알림
+    @MessageMapping("/study-notice/alarm/{studyId}")
+    public void studyNoticeAlarm(
+            @PathVariable Long studyId,
+            @AuthenticationPrincipal User user
+    ) {
+        studyNoticeService.sendStudyNoticeAlarm(studyId, user);
     }
 }
--- src/main/java/com/example/studyplatform/service/studyNotice/StudyNoticeService.java
+++ src/main/java/com/example/studyplatform/service/studyNotice/StudyNoticeService.java
@@ -1,10 +1,13 @@
 package com.example.studyplatform.service.studyNotice;
 
+import com.example.studyplatform.domain.alarm.Alarm;
+import com.example.studyplatform.domain.alarm.AlarmRepository;
 import com.example.studyplatform.domain.study.Study;
 import com.example.studyplatform.domain.study.StudyRepository;
 import com.example.studyplatform.domain.studyNotice.StudyNotice;
 import com.example.studyplatform.domain.studyNotice.StudyNoticeRepository;
 import com.example.studyplatform.domain.user.User;
+import com.example.studyplatform.dto.alarm.AlarmRequest;
 import com.example.studyplatform.dto.response.Response;
 import com.example.studyplatform.dto.studyNotice.PostStudyNoticeRequest;
 import com.example.studyplatform.dto.studyNotice.PutStudyNoticeRequest;
@@ -12,6 +15,8 @@ import com.example.studyplatform.dto.studyNotice.StudyNoticeResponse;
 import com.example.studyplatform.exception.StudyNotFoundException;
 import com.example.studyplatform.exception.StudyNoticeNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +30,9 @@ public class StudyNoticeService {
 
     private final StudyRepository studyRepository;
     private final StudyNoticeRepository studyNoticeRepository;
+    private final RedisTemplate redisTemplate;
+    private final ChannelTopic channelTopic;
+    private final AlarmRepository alarmRepository;
 
     public List<StudyNoticeResponse> list(Long studyId) {
         List<StudyNotice> studyNotices = studyNoticeRepository.findAllByStudyId(studyId);
@@ -40,6 +48,24 @@ public class StudyNoticeService {
         StudyNotice studyNotice = studyNoticeRepository.save(req.toEntity(study, user));
 
         return studyNotice.result();
+    }
+
+    @Transactional
+    public void sendStudyNoticeAlarm(Long studyId, User user) {
+        Study study = studyRepository.findById(studyId).orElseThrow(StudyNotFoundException::new);
+
+        String topic = channelTopic.getTopic();
+        String title = user.getNickname() + "님이 " + study.getStudyName() + "에 공지를 남겼습니다.";
+
+        study.getStudyUsers().forEach(u -> {
+            Alarm alarm = alarmRepository.save(Alarm.builder()
+                    .title(title)
+                    .url("studyURL")
+                    .user(u.getUser())
+                    .build());
+
+            redisTemplate.convertAndSend(topic, AlarmRequest.toDto(alarm, u.getUser().getId()));
+        });
     }
 
     @Transactional
