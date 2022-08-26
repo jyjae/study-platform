751b16a86119180b8913b7f1d522d92fab35557b
yeonjaery <yeonjaery@google.com>
2022-08-27 01:39:20 +0900

    Feat: user에 providerName 추가, oauth 관련 쿼리메소드 추가

--- src/main/java/com/example/studyplatform/domain/user/UserRepository.java
+++ src/main/java/com/example/studyplatform/domain/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.studyplatform.domain.user;
 
 import com.example.studyplatform.constant.Status;
+import com.example.studyplatform.constant.oauth.ProviderName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,4 +14,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailAndStatus(String email, Status active);
 
     Optional<User> findByNicknameAndStatus(String name, Status active);
+
+    int existsByEmailAndProviderNameAndStatus(String email, ProviderName kakao, Status active);
+
+
+    Optional<User> findByEmailAndProviderNameAndStatus(String email, ProviderName kakao, Status active);
 }
