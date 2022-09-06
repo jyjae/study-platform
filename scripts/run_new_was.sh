ea1921eec68c4820f253800f50d4aedc6ded7480
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2022-09-06 18:43:43 +0900

    ci/cd 테스트

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -22,6 +22,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill -9 ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/study-platform/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+sudo nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/study-platform/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
