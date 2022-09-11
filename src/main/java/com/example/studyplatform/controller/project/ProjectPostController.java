ce22eb77743a1dd23fcdc97cccf7a112c931df30
dmin3 <ekdrnzld@gmail.com>
2022-09-11 18:11:21 +0900

    fix : 프로젝트 API 명세서 추가

--- src/main/java/com/example/studyplatform/controller/project/ProjectPostController.java
+++ src/main/java/com/example/studyplatform/controller/project/ProjectPostController.java
@@ -2,22 +2,39 @@ package com.example.studyplatform.controller.project;
 
 import com.example.studyplatform.domain.user.User;
 import com.example.studyplatform.dto.project.ProjectPostCreateRequest;
+import com.example.studyplatform.dto.project.ProjectPostResponse;
 import com.example.studyplatform.dto.project.ProjectPostUpdateRequest;
 import com.example.studyplatform.dto.response.Response;
 import com.example.studyplatform.service.project.ProjectPostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
+@Tag(name = "project-post-controller", description = "프로젝트 게시물 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/project-post")
 @RestController
 public class ProjectPostController {
     private final ProjectPostService projectPostService;
 
+    @Operation(
+            summary = "프로젝트 게시물 생성",
+            description = "프로젝트 게시물을 생성합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "프로젝트게시물 생성 성공", content = @Content(schema = @Schema(implementation = Response.class)))
+            },
+            parameters = {
+                    @Parameter(name = "X-ACCESS-TOKEN", description = "JWT 토큰", in = ParameterIn.HEADER)
+            }
+    )
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Response create(
@@ -28,12 +45,32 @@ public class ProjectPostController {
         return Response.success();
     }
 
+    @Operation(
+            summary = "프로젝트 게시물 상세 조회",
+            description = "프로젝트 게시물을 상세 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "프로젝트 게시물 조회 성공", content = @Content(schema = @Schema(implementation = ProjectPostResponse.class))),
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "프로젝트 게시물 ID", in = ParameterIn.PATH),
+            }
+    )
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Response read(@PathVariable Long id) {
         return Response.success(projectPostService.read(id));
     }
 
+    @Operation(
+            summary = "프로젝트 게시물 삭제",
+            description = "프로젝트 게시물을 삭제 합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "프로젝트 게시물 삭제 성공", content = @Content(schema = @Schema(implementation = Response.class))),
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "프로젝트 게시물 ID", in = ParameterIn.PATH),
+            }
+    )
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Response delete(@PathVariable Long id) {
@@ -41,6 +78,16 @@ public class ProjectPostController {
         return Response.success();
     }
 
+    @Operation(
+            summary = "프로젝트 게시물 수정",
+            description = "프로젝트 게시물을 수정 합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "프로젝트 게시물 수정 성공", content = @Content(schema = @Schema(implementation = Response.class))),
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "프로젝트 게시물 ID", in = ParameterIn.PATH),
+            }
+    )
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Response update(
--- src/main/java/com/example/studyplatform/controller/project/ProjectResumeController.java
+++ src/main/java/com/example/studyplatform/controller/project/ProjectResumeController.java
@@ -1,22 +1,38 @@
 package com.example.studyplatform.controller.project;
 
 import com.example.studyplatform.domain.user.User;
-import com.example.studyplatform.dto.project.ProjectResumeApprovalRequest;
-import com.example.studyplatform.dto.project.ProjectResumeCreateRequest;
-import com.example.studyplatform.dto.project.ProjectResumeDeleteRequest;
+import com.example.studyplatform.dto.project.*;
 import com.example.studyplatform.dto.response.Response;
 import com.example.studyplatform.service.project.ProjectResumeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "project-resume-controller", description = "프로젝트 신청서 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/project-resume")
 @RestController
 public class ProjectResumeController {
     private final ProjectResumeService projectResumeService;
 
+    @Operation(
+            summary = "프로젝트 신청서 생성",
+            description = "프로젝트 신청서를 생성합니다",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "프로젝트 신청서 생성 성공", content = @Content(schema = @Schema(implementation = Response.class)))
+            },
+            parameters = {
+                    @Parameter(name = "X-ACCESS-TOKEN", description = "JWT 토큰", in = ParameterIn.HEADER)
+            }
+    )
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Response create(
@@ -27,12 +43,32 @@ public class ProjectResumeController {
         return Response.success();
     }
 
+    @Operation(
+            summary = "프로젝트 신청서 조회",
+            description = "프로젝트 신청서를 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "프로젝트 신청서 조회 성공", content = @Content(schema = @Schema(implementation = ProjectResumeResponse.class))),
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "프로젝트 신청서 ID", in = ParameterIn.PATH)
+            }
+    )
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Response read(@PathVariable("id") Long resumeId) {
         return Response.success(projectResumeService.read(resumeId));
     }
 
+    @Operation(
+            summary = "프로젝트 신청서 승인",
+            description = "프로젝트 신청서를 승인합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "프로젝트 신청서 승인 성공", content = @Content(schema = @Schema(implementation = Response.class))),
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "프로젝트 신청서 ID", in = ParameterIn.PATH)
+            }
+    )
     @PatchMapping("/{id}/approval")
     @ResponseStatus(HttpStatus.OK)
     public Response approvalResume(
@@ -43,6 +79,16 @@ public class ProjectResumeController {
         return Response.success();
     }
 
+    @Operation(
+            summary = "프로젝트 신청서 거절",
+            description = "프로젝트 신청서를 거절합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "프로젝트 신청서 거절 성공", content = @Content(schema = @Schema(implementation = Response.class))),
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "프로젝트 신청서 ID", in = ParameterIn.PATH)
+            }
+    )
     @PatchMapping("/{id}/denied")
     @ResponseStatus(HttpStatus.OK)
     public Response deniedResume(
@@ -52,6 +98,16 @@ public class ProjectResumeController {
         return Response.success();
     }
 
+    @Operation(
+            summary = "확정된 프로젝트 신청서 삭제",
+            description = "확정된 프로젝트 신청서를 삭제합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "확정된 프로젝트 신청서 삭제 성공", content = @Content(schema = @Schema(implementation = Response.class))),
+            },
+            parameters = {
+                    @Parameter(name = "id", description = "프로젝트 신청서 ID", in = ParameterIn.PATH)
+            }
+    )
     @PatchMapping("/{id}/delete")
     @ResponseStatus(HttpStatus.OK)
     public Response deleteAppliedResume(
