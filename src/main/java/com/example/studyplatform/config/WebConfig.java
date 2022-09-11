8dee7e58ad5585acb6c44d33cbb8d07b2de49240
yeonjaery <yeonjaery@google.com>
2022-09-11 13:38:04 +0900

    feat: cors 일단 모두 허용

---
+++ src/main/java/com/example/studyplatform/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.example.studyplatform.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+}
